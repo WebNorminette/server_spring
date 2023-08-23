1948666e801b311352db7dc5934bcb1859451508
Rillmo <maxman306@gmail.com>
2023-08-24 01:59:52 +0900

    프로젝트 세팅

---
+++ prototype-ver1/prototype-ver1/src/main/java/com/webnorm/prototypever1/PrototypeVer1Application.java
@@ -0,0 +1,12 @@
+package com.webnorm.prototypever1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeVer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeVer1Application.class, args);
+	}
+}
---
+++ prototype-ver1/prototype-ver1/src/test/java/com/webnorm/prototypever1/PrototypeVer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webnorm.prototypever1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeVer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
